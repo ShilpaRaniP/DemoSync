ca8826725448b35fda84ec29fb692867ca22cf4a
smart <smart@Shilpa>
2025-03-04 12:55:09 +0530

    created branch version1.2.0

--- DemoSync12378/src/test/java/pac3/OrderTest.java
+++ DemoSync12378/src/test/java/pac3/OrderTest.java
@@ -9,4 +9,10 @@ public class OrderTest {
 		System.out.println("executing createOrderTest");
 	}
 
+	
+	@Test
+	public void updateOrderTest()
+	{
+		System.out.println("executing updateOrderTest");
+	}
 }
