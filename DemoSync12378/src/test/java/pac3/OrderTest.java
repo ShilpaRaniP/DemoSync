debf38902b018f775fb2db8d13e9430da7b882ae
smart <smart@Shilpa>
2025-03-04 12:41:56 +0530

    added pac3

---
+++ DemoSync12378/src/test/java/pac3/OrderTest.java
@@ -0,0 +1,12 @@
+package pac3;
+
+import org.testng.annotations.Test;
+
+public class OrderTest {
+	@Test
+	public void createOrderTest()
+	{
+		System.out.println("executing createOrderTest");
+	}
+
+}
