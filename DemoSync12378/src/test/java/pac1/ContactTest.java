59a3735afada2fb04ab997c246b45239ed84d577
smart <smart@Shilpa>
2025-03-04 12:20:53 +0530

    first push

---
+++ DemoSync12378/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,19 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createContact()
+	{
+		System.out.println(" executed createContact");
+	}
+
+	
+	@Test
+	public void createContactWithPhoneNo()
+	{
+		System.out.println(" executed createContactWithPhoneNo");
+	}
+}
