4cdb1f9a3c4aa3438f5c41ff7f85ea7addb8519d
smart <smart@Shilpa>
2025-03-05 12:08:03 +0530

    removed one method in ordertest

--- DemoSync12378/src/test/java/pac3/OrderTest.java
+++ DemoSync12378/src/test/java/pac3/OrderTest.java
@@ -9,16 +9,5 @@ public class OrderTest {
 		System.out.println("executing createOrderTest");
 	}
 
-	@Test
-	public void updateOrderTest()
-	{
-		System.out.println("executing updateOrderTest");
-	}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'Version1.2.0' of https://github.com/ShilpaRaniP/DemoSync.git
->>>>>>> branch 'Version1.2.0' of https://github.com/ShilpaRaniP/DemoSync.git
+	
 }
